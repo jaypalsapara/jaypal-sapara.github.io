1719b6de43db4da9e22ffa26f09cccd8137db43c
Jaypal Sapara <48550828+jaypal-sapara@users.noreply.github.com>
2025-04-17 13:18:01 +0530

    Update

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -3,6 +3,13 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     build: {
         outDir: '../', // Replace with the folder name you want
+        rollupOptions: {
+            input: './index.html', // Specify the entry point
+            output: {
+                entryFileNames: 'assets/[name].js',
+                assetFileNames: info => info.names == 'index.css' ? 'assets/[name].[ext]' : 'assets/[name]-[hash].[ext]', // Keep asset names unchanged
+            },
+        },
     },
     plugins: [
         tailwindcss(),
